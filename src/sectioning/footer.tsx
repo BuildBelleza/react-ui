999551a31cc1de3de3e5ddff6e91a74ba26791fa
BuildBelleza <123292150+BuildBelleza@users.noreply.github.com>
2023-08-27 07:38:54 -0400

    update

--- src/sectioning/footer.tsx
+++ src/sectioning/footer.tsx
@@ -1,6 +1,18 @@
 const Footer = (): JSX.Element => (
   <footer>
-    <div className="y-wrap">Footer</div>
+    <div className="y-wrap">
+      <div className="footerContainer">
+    <div className="footer">
+      <h3>Hawkins Custom Tile</h3>
+      
+      
+      
+      <div className="copyright">
+        &copy; Copyright <strong><span>2023</span></strong>. All Rights Reserved
+      </div>
+      </div>
+      </div>
+    </div>
   </footer>
 );
 
